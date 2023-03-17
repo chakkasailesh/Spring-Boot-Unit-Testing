a1706dda5b18f04b113596c9da499824159f6747
chakkasailesh <chakkasailesh444@gmail.com>
2023-03-17 23:29:29 +0530

    added controller tests

--- spring-boot-unit-testing/src/main/java/com/springbootunittesting/controller/EmployeeController.java
+++ spring-boot-unit-testing/src/main/java/com/springbootunittesting/controller/EmployeeController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -33,5 +37,22 @@ public class EmployeeController {
 	@GetMapping
 	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Employee> getEmployee(@PathVariable Long id) {
+		return employeeService.getEmployee(id).map(ResponseEntity::ok)
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employee) {
+		return ResponseEntity.ok(employeeService.updateEmployee(id, employee));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteEmployee(@PathVariable Long id) {
+		employeeService.deleteEmployee(id);
+		return ResponseEntity.ok("Employee with id " + id + " deleted successfully");
 	}
 }
--- spring-boot-unit-testing/src/test/java/com/springbootunittesting/controller/EmployeeControllerTests.java
+++ spring-boot-unit-testing/src/test/java/com/springbootunittesting/controller/EmployeeControllerTests.java
@@ -3,13 +3,17 @@ package com.springbootunittesting.controller;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -24,6 +28,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.springbootunittesting.entity.Employee;
+import com.springbootunittesting.exception.ResourceNotFoundException;
 import com.springbootunittesting.service.EmployeeService;
 
 @WebMvcTest
@@ -77,6 +82,88 @@ public class EmployeeControllerTests {
 
 		// then - verify the output
 		response.andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$.size()", is(2)));
+	}
+
+	@Test
+	@DisplayName("Test for getEmployee Rest API")
+	public void givenEmploeeId_whenGetEmployee_thenReturnEmployee() throws Exception {
+		// given - precondition or setup
+		long id = 1L;
+		given(employeeService.getEmployee(id)).willReturn(Optional.of(employee));
+
+		// when - action or behavior that we are going to test
+		ResultActions response = mockMvc.perform(get("/api/employees/{id}", id));
+
+		// then - verify the output
+		response.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.firstName", is(employee.getFirstName())))
+				.andExpect(jsonPath("$.lastName", is(employee.getLastName())))
+				.andExpect(jsonPath("$.email", is(employee.getEmail())));
+	}
+
+	@Test
+	@DisplayName("Test for getEmployee Rest API negative case")
+	public void givenInvalidEmployeeId_whenGetEmployee_thenReturnNotFound() throws Exception {
+		// given - precondition or setup
+		long id = 3L;
+		given(employeeService.getEmployee(id)).willReturn(Optional.empty());
+
+		// when - action or behavior that we are going to test
+		ResultActions response = mockMvc.perform(get("/api/employees/{id}", id));
+
+		// then - verify the output
+		response.andDo(print()).andExpect(status().isNotFound());
+	}
+
+	@Test
+	@DisplayName("Test for updateEmployee Rest API")
+	public void givenEmployeeId_whenUpdateEmployee_thenReturnUpdatedEmployee() throws Exception {
+		// given - precondition or setup
+		Employee employee1 = Employee.builder().firstName("Robert").lastName("Downey Jr").email("tonystark@email.com")
+				.build();
+		Long id = 1L;
+		given(employeeService.updateEmployee(id, employee1)).willReturn(employee1);
+
+		// when - action or behavior that we are going to test
+		ResultActions response = mockMvc.perform(put("/api/employees/{id}", id).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(employee1)));
+
+		// then - verify the output
+		response.andDo(print()).andExpect(status().isOk())
+				.andExpect(jsonPath("$.firstName", is(employee1.getFirstName())))
+				.andExpect(jsonPath("$.lastName", is(employee1.getLastName())))
+				.andExpect(jsonPath("$.email", is(employee1.getEmail())));
+	}
+
+	@Test()
+	@DisplayName("Test for updateEmployee Rest API negative case")
+	public void givenInvalidEmployeeId_whenUpdateEmployee_thenReturnNotFound() throws Exception {
+		// given - precondition or setup
+		Employee employee1 = Employee.builder().firstName("Robert").lastName("Downey Jr").email("tonystark@email.com")
+				.build();
+		Long id = 1L;
+		given(employeeService.updateEmployee(id, employee1)).willThrow(ResourceNotFoundException.class);
+
+		// when - action or behavior that we are going to test
+		ResultActions response = mockMvc.perform(put("/api/employees/{id}", id).contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(employee1)));
+
+		// then - verify the output
+		response.andDo(print()).andExpect(status().isNotFound());
+	}
+
+	@Test()
+	@DisplayName("Test for deleteEmployee Rest API")
+	public void givenEmployeeId_whenDeleteEmployee_thenReturnSuccessMessage() throws Exception {
+		// given - precondition or setup
+		Long id = 1L;
+		willDoNothing().given(employeeService).deleteEmployee(id);
+
+		// when - action or behavior that we are going to test
+		ResultActions response = mockMvc.perform(delete("/api/employees/{id}", id));
+
+		// then - verify the output
+		response.andDo(print()).andExpect(status().isOk());
 	}
 
 }
